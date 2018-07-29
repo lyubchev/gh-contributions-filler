de2c0dd892916143ee3263664236aac9cba555a3
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-07-29 13:37:13 +0300

    Display information on screen about the project

---
+++ gh-contributions-filler/gh-contributions-filler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace gh_contributions_filler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            Console.BufferHeight = 30;
+            Console.WriteLine(@"
+   _____   _   _     _    _           _     
+  / ____| (_) | |   | |  | |         | |    
+ | |  __   _  | |_  | |__| |  _   _  | |__  
+ | | |_ | | | | __| |  __  | | | | | | '_ \ 
+ | |__| | | | | |_  | |  | | | |_| | | |_) |
+  \_____| |_|  \__| |_|  |_|  \__,_| |_.__/ 
+
+ CALENDAR FILLER V1.0
+");         
+            Console.WriteLine(@"Type ""help"" for commands" + Environment.NewLine);
+            userInput = Console.ReadLine();
+            if(userInput == "help")
+            {
+                Console.WriteLine(@" run help - Gives information about starting a fill session");
+                Console.WriteLine(@" run fill - Starts new session");
+                Console.WriteLine(@" end fill - Ends current session");
+                Console.WriteLine(@" about - Gives information about the project");
+                Console.WriteLine(@" about auth - Gives information about the author");
+            }
+        }
+    }
+}
